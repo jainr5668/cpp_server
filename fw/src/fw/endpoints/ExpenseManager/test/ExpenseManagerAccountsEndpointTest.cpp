824ea27293586ec7d49832c78a66a217531d2d9e
Ronak Jain <ronak.jain@hp.com>
2024-11-25 19:26:05 +0530

    initial testing

---
+++ fw/src/fw/endpoints/ExpenseManager/test/ExpenseManagerAccountsEndpointTest.cpp
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <MockIUtilityService.h>
+#include <ExpenseManagerInjections.h>
+#include <ExpenseManagerAccountsEndpoint.h>
+
+class ExpenseManagerAccountsEndpointTest : public ::testing::Test {
+protected:
+    virtual void SetUp() override;
+    virtual void TearDown() override;
+    endpoints::ExpenseManager::ExpenseManagerInjections* injections_;
+    endpoints::ExpenseManager::ExpenseManagerAccountsEndpoint* endpoint;
+    RouteContext routeContext;
+};
+
+void ExpenseManagerAccountsEndpointTest::SetUp(){
+    injections_ = new endpoints::ExpenseManager::ExpenseManagerInjections();
+    injections_->utilityService = std::make_shared<services::CommonService::MockIUtilityService>();
+    endpoint = new endpoints::ExpenseManager::ExpenseManagerAccountsEndpoint(injections_);
+    routeContext.req = new Request();
+    routeContext.res = new Response();
+}
+void ExpenseManagerAccountsEndpointTest::TearDown(){
+    delete injections_;
+    delete endpoint;
+}
+
+TEST_F(ExpenseManagerAccountsEndpointTest, WhenAddAccountIsCalled_ThenReturn500){
+    endpoint->addAccount(routeContext);
+    EXPECT_EQ(routeContext.res->status_code, 500);
+    EXPECT_EQ(routeContext.res->body, "Not Implemented");
+}
+
+TEST_F(ExpenseManagerAccountsEndpointTest, WhenGetAccountIsCalled_ThenReturn500){
+    endpoint->getAccount(routeContext);
+    EXPECT_EQ(routeContext.res->status_code, 500);
+    EXPECT_EQ(routeContext.res->body, "Not Implemented");
+}
+
+TEST_F(ExpenseManagerAccountsEndpointTest, WhenGetAccountsIsCalled_ThenReturn500){
+    endpoint->getAccounts(routeContext);
+    EXPECT_EQ(routeContext.res->status_code, 500);
+    EXPECT_EQ(routeContext.res->body, "Not Implemented");
+}
+
+TEST_F(ExpenseManagerAccountsEndpointTest, WhenUpdateAccountsIsCalled_ThenReturn500){
+    endpoint->updateAccount(routeContext);
+    EXPECT_EQ(routeContext.res->status_code, 500);
+    EXPECT_EQ(routeContext.res->body, "Not Implemented");
+}
+
+TEST_F(ExpenseManagerAccountsEndpointTest, WhenDeleteAccountsIsCalled_ThenReturn500){
+    endpoint->deleteAccount(routeContext);
+    EXPECT_EQ(routeContext.res->status_code, 500);
+    EXPECT_EQ(routeContext.res->body, "Not Implemented");
+}
---
+++ fw/src/fw/endpoints/ExpenseManager/test/ExpenseManagerTagsEndpointTest.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <MockIUtilityService.h>
+#include <ExpenseManagerInjections.h>
+#include "ExpenseManagerTagsEndpoint.h"
+
+class ExpenseManagerTagsEndpointTest : public ::testing::Test {
+protected:
+    virtual void SetUp() override;
+    virtual void TearDown() override;
+    endpoints::ExpenseManager::ExpenseManagerInjections* injections_;
+    endpoints::ExpenseManager::ExpenseManagerTagsEndpoint* endpoint;
+    RouteContext routeContext;
+};
+
+void ExpenseManagerTagsEndpointTest::SetUp(){
+    injections_ = new endpoints::ExpenseManager::ExpenseManagerInjections();
+    injections_->utilityService = std::make_shared<services::CommonService::MockIUtilityService>();
+    endpoint = new endpoints::ExpenseManager::ExpenseManagerTagsEndpoint(injections_);
+    routeContext.req = new Request();
+    routeContext.res = new Response();
+}
+void ExpenseManagerTagsEndpointTest::TearDown(){
+    delete injections_;
+    delete endpoint;
+}
+
+TEST_F(ExpenseManagerTagsEndpointTest, WhenAddTagIsCalled_ThenReturn500){
+    endpoint->addTag(routeContext);
+    EXPECT_EQ(routeContext.res->status_code, 500);
+    EXPECT_EQ(routeContext.res->body, "Not Implemented");
+}
+
+TEST_F(ExpenseManagerTagsEndpointTest, WhenGetTagsIsCalled_ThenReturn500){
+    endpoint->getTags(routeContext);
+    EXPECT_EQ(routeContext.res->status_code, 500);
+    EXPECT_EQ(routeContext.res->body, "Not Implemented");
+}
+
+TEST_F(ExpenseManagerTagsEndpointTest, WhenUpdateTagsIsCalled_ThenReturn500){
+    endpoint->updateTag(routeContext);
+    EXPECT_EQ(routeContext.res->status_code, 500);
+    EXPECT_EQ(routeContext.res->body, "Not Implemented");
+}
+
+TEST_F(ExpenseManagerTagsEndpointTest, WhenDeleteTagsIsCalled_ThenReturn500){
+    endpoint->deleteTag(routeContext);
+    EXPECT_EQ(routeContext.res->status_code, 500);
+    EXPECT_EQ(routeContext.res->body, "Not Implemented");
+}
