ffb59f1c4f5a307608bc9ae779cb82156cfde5da
Ronak Jain <ronak.jain@hp.com>
2024-12-14 21:54:54 +0530

    EWS UI update initial

--- ews/src/app/features/auth/login/login.component.ts
+++ ews/src/app/features/auth/login/login.component.ts
@@ -87,7 +87,7 @@ export class LoginComponent implements OnInit {
           if (this.returnUrl) {
             this.router.navigate([this.returnUrl]);
           } else {
-            // this.router.navigate(['']);
+            this.router.navigate(['']);
           }
         },
         error: (error) => {
